efeb96222d4711b095b48dcdf95078334f258e76
guyuqing <1260329072@qq.com>
2017-12-25 16:39:44 +0800

    【mod】流水列表查询条件增加

--- src/services/admin/order/taskseq.js
+++ src/services/admin/order/taskseq.js
@@ -3,8 +3,10 @@
 import adminServer from '../../../config/server';
 
 const Taskseq = {
-
+  // 获取流水列表
   list: (params) => Promise.resolve(adminServer.get('/api/taskflow/flow/page', { params: params })),
+  // 根据门店号获取设计师
+  designer: (bid) => Promise.resolve(adminServer.get(`/api/store/employee/designer/${bid}`)),
 
 };
 
