f3900f68b630479e5e7b85297cba56b5013b2c44
guyuqing <1260329072@qq.com>
2017-12-28 14:53:18 +0800

    【mod】services

--- src/services/admin/basic/material.js
+++ src/services/admin/basic/material.js
@@ -1,4 +1,4 @@
-// import qs from 'qs';
+import qs from 'qs';
 
 import adminServer from '../../../config/server';
 
@@ -7,6 +7,10 @@ const Material = {
   list: (params) => Promise.resolve(adminServer.get('/api/material', { params: params })),
   // 物料分类列表
   typeList: (params) => Promise.resolve(adminServer.get('/api/material/type/list', { params: params })),
+  // 修改物料分类
+  editType: (params) => Promise.resolve(adminServer.put(`/api/material/type/${params.id}`, qs.stringify(params))),
+  // 修改物料分类状态
+  deleteType: (id) => Promise.resolve(adminServer.put(`/api/material/type/stcode/${id}?stcode=0`)),
 
 };
 
