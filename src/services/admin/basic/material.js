aab9ddfa622339075908fdc4bc706213474e5e50
guyuqing <1260329072@qq.com>
2017-12-28 17:54:00 +0800

    【mod】物料列表

--- src/services/admin/basic/material.js
+++ src/services/admin/basic/material.js
@@ -7,6 +7,10 @@ const Material = {
   list: (params) => Promise.resolve(adminServer.get('/api/material', { params: params })),
   // 物料分类列表
   typeList: (params) => Promise.resolve(adminServer.get('/api/material/type/list', { params: params })),
+  // 获取物料分类详情
+  detailType: (id) => Promise.resolve(adminServer.get(`/api/material/type/${id}`)),
+  // 新增物料分类
+  addType: (params) => Promise.resolve(adminServer.post('/api/material/type', qs.stringify(params))),
   // 修改物料分类
   editType: (params) => Promise.resolve(adminServer.put(`/api/material/type/${params.id}`, qs.stringify(params))),
   // 修改物料分类状态
--- src/views/basic/material/list.vue
+++ src/views/basic/material/list.vue
@@ -2,103 +2,205 @@
   <div class="dis-flex container">
     <div class="dis-flex"> 
       <div class="list-option">
-        <screening :screening="screening" @submit="query"></screening>
-        <div class="page-oper">
-          <div class="page-title">员工列表</div>
-          <ul class="page-methods">
-            <li>
-              <el-button type="primary" @click="edit()">新增员工</el-button>
-            </li>
-            <li>
-              <el-button type="primary" @click="check()">查看所有部门信息</el-button>
-            </li>
-          </ul>
-        </div>
+        <el-row>
+          <el-col :span="4">
+            <h1 class="material-title">物料管理</h1>
+          </el-col>
+          <el-col :span="20">
+            <screening :screening="screening" @submit="query" ></screening>
+          </el-col>
+        </el-row>       
       </div>
-      <div class="dis-flex" v-loading.lock="loading">
-        <div class="table dis-flex">
-          <div class="admin-table dis-flex">
-            <el-checkbox-group v-model="checkList">
-              <table class="admin-main-table">
-                <thead>
-                  <tr>
-                    <th>序号</th>
-                    <th v-for="value in thead" :title="value">
-                      {{value}}
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="(item, index) in tbody">
-                    <td>
-                      {{((conditions.pageNo - 1) * conditions.pageSize) + index + 1}}
-                    </td>
-                    <td class="router"><span @click="detail(item.id)">{{item.id}}</span></td>
-                    <td>{{item.name}}</td>
-                    <td>{{item.mobile}}</td>              
-                    <td>{{item.deptName}}</td>
-                    <td>{{item.organName}}</td>
-                    <td>
-                      <span v-for="data in item.roleList">
-                        {{data}}
-                      </span>
-                    </td>
-                    <td>{{item.stateName}}</td>
-                  </tr>
-                  <tr v-if="tbody.length==0 && !loading">
-                    <td :colspan="thead.length + 1" class="nothing-data">暂无数据</td>
-                  </tr>
-                </tbody>
-              </table>
-            </el-checkbox-group>
-          </div>
-        </div>
-        <div class="pagination">
-          <el-pagination
-              @size-change="handleSizeChange"
-              @current-change="handleCurrentChange"
-              :current-page="paginationData.page"
-              :page-size="paginationData.pageSize"
-              layout="total, sizes, prev, pager, next, jumper"
-              :total="paginationData.totalItems">
-          </el-pagination>
-        </div>
-      </div>
+
+      <div class="dis-flex">
+          <el-row>
+            <el-col :span="3">
+              <el-col :span="23">
+              <div class="tree-wrapper">
+                <div class="tree-button">
+                  <el-button  icon="plus" @click="addType" class="plus-btn"></el-button>
+                  <el-button  icon="edit" @click="editType" class="edit-btn"></el-button>
+                  <el-button  icon="delete" @click="deleteType" class="delete-btn"></el-button>
+                  <el-dialog :title="typeForm.options.title" :visible.sync="typeForm.dialogFormVisible">
+                    <el-form :model="typeForm.form" ref="ruleForm" :rules="typeForm.rules">
+                      <el-form-item label="所属分类：" :label-width="typeForm.formLabelWidth" v-if="typeForm.create">
+                        <el-cascader
+                          :options="typeForm.types"
+                          :value="typeForm.types.name"
+                          :props="typeForm.props"
+                          change-on-select
+                          @change="addTypeId"
+                        ></el-cascader>
+                      </el-form-item>
+                      <el-form-item label="类别名称：" :label-width="typeForm.formLabelWidth" prop="name">
+                        <el-input v-model="typeForm.form.name"></el-input>
+                      </el-form-item>
+                      <el-form-item label="物料分类编码：" :label-width="typeForm.formLabelWidth" prop="no">
+                        <el-input v-model="typeForm.form.no"></el-input>
+                      </el-form-item>
+                      <el-form-item label="物料编码规则：" :label-width="typeForm.formLabelWidth" prop="rule">
+                        <el-input v-model="typeForm.form.rule"></el-input>
+                      </el-form-item>
+                      <el-form-item label="助记码：" :label-width="typeForm.formLabelWidth">
+                        <el-input v-model="typeForm.form.searchNo"></el-input>
+                      </el-form-item>
+                      <el-form-item label="备注" :label-width="typeForm.formLabelWidth">
+                        <el-input v-model="typeForm.form.remark"></el-input>
+                      </el-form-item>
+                    </el-form>
+                    <div slot="footer" class="dialog-footer">
+                      <el-button @click="typeForm.dialogFormVisible = false">取 消</el-button>
+                      <el-button type="primary" @click="typeSave('ruleForm')">确 定</el-button>
+                    </div>
+                  </el-dialog>
+                </div>
+                <el-tree 
+                  ref="tree"
+                  :data="treeData"
+                  :props="defaultProps"               
+                  @node-click="handleNodeClick"                                 
+                  highlight-current>
+                </el-tree>
+              </div>
+              </el-col>
+            </el-col>
+            <el-col :span="21">
+              <div class="dis-flex">
+                <div class="page-oper">
+                  <div class="page-title">物料列表</div>
+                  <ul class="page-methods">
+                    <li>
+                      <el-button type="primary">新建</el-button>
+                    </li>
+                    <li>
+                      <el-button type="success">编辑</el-button>
+                    </li>
+                    <li>
+                      <el-button type="danger">禁用</el-button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="dis-flex" v-loading.lock="loading">
+                <div class="table dis-flex">
+                  <div class="admin-table dis-flex">
+                    <el-checkbox-group>
+                      <table class="admin-main-table">
+                        <thead>
+                          <tr>
+                            <th>序号</th>
+                            <th v-for="value in thead" :title="value">
+                              {{value}}
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="(item, index) in tbody">
+                            <td>
+                              {{((conditions.pageNo - 1) * conditions.pageSize) + index + 1}}
+                            </td>
+                            <td>{{item.no}}</td>
+                            <td>{{item.name}}</td>
+                            <td>{{item.standard}}</td>              
+                            <td>{{item.defaultUnitId}}</td>
+                            <td>{{item.purchasableName}}</td>
+                            <td>{{item.marketableName}}</td>
+                            <td>{{item.homemadeName}}</td>
+                          </tr>
+                          <tr v-if="tbody.length==0 && !loading">
+                            <td :colspan="thead.length + 1" class="nothing-data">暂无数据</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </el-checkbox-group>
+                  </div>
+                </div>
+                <div class="pagination">
+                  <el-pagination
+                      @size-change="handleSizeChange"
+                      @current-change="handleCurrentChange"
+                      :current-page="paginationData.page"
+                      :page-size="paginationData.pageSize"
+                      layout="total, sizes, prev, pager, next, jumper"
+                      :total="paginationData.totalItems">
+                  </el-pagination>
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </div>    
     </div>
   </div>
 </template>
 
 <script>
 import screening from '../../../components/screening.vue';
-import { Employees } from '../../../services/admin';
+import { Material } from '../../../services/admin';
 
 export default {
   data() {
     return {
-      thead: ['员工编号', '员工名称', '手机号', '所属部门', '所属机构', '员工角色', '账号状态'],
+      thead: ['物料编号', '物料名称', '物料规格', '单位', '可外购', '可销售', '可自制'],
       tbody: [],
       screening: [
         [
           {
-            label: '员工姓名',
+            label: '物料名称',
             type: 'input',
             field: 'name',
-          },
-          {
-            label: '员工手机',
-            type: 'input',
-            field: 'mobile',
           },
         ],
       ],
       loading: true,
       paginationData: {},
-      checkList: [],
       conditions: {
         pageSize: '',
         pageNo: '',
         did: this.$route.query.did,
         bid: this.$route.query.bid,
+      },
+      treeData: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name',
+        isLeaf: 'stCode',
+      },
+      typeForm: {
+        typeParams: {},
+        dialogFormVisible: false,
+        create: 'true',
+        form: {
+          name: '',
+          id: '',
+          parentId: '',
+          rule: '',
+          remark: '',
+          searchNo: '',
+        },
+        options: {
+          type: 'addType',
+          message: '新增类别',
+          btn: '确认新增',
+          title: '新增类别',
+        },
+        props: {
+          value: 'id',
+          label: 'name',
+          children: 'children',
+        },
+        formLabelWidth: '140px',
+        types: [],
+        rules: {
+          name: [
+            { required: true, message: '请填写名称', trigger: 'blur' },
+          ],
+          no: [
+            { required: true, message: '请填写编码', trigger: 'blur' },
+          ],
+          rule: [
+            { required: true, message: '请填写编码规则', trigger: 'blur' },
+          ],
+        },
       },
     };
   },
@@ -112,26 +214,145 @@ export default {
   methods: {
     init: function (val) {
       this.loading = true;
-      if (!this.$route.query.bid) {
-        Employees.list(val).then(res => {
-          this.loading = false;
-          this.paginationData = res.data;
-          this.tbody = res.data.result;
-          this.conditions.pageSize = res.data.pageSize;
-          this.conditions.pageNo = res.data.page;
+      this.getList(val);
+      Material.typeList().then(res => {
+        this.loading = false;
+        this.treeData = res.data;
+      }).catch(err => {
+        console.log(err);
+      });
+    },
+    getList(params) {
+      Material.list(params).then(res => {
+        this.loading = false;
+        this.paginationData = res.data;
+        this.tbody = res.data.result;
+        this.conditions.pageSize = res.data.pageSize;
+        this.conditions.pageNo = res.data.page;
+      }).catch(err => {
+        console.log(err);
+      });
+    },
+    addTypeId: function (val) {
+      console.log(val);
+      console.log(val[0]);
+      if (val.length === 2) {
+        const a = val[1];
+        this.typeForm.form.parentId = a;
+      } else {
+        const a = val[0];
+        this.typeForm.form.parentId = a;
+      }
+      console.log('parentId', this.typeForm.form.parentId);
+    },
+    addType: function () {
+      console.log('treeData', this.treeData);
+      this.typeForm.types = this.treeData;
+      this.filterInfo(this.typeForm.types);
+      console.log('types', this.typeForm.types);
+      this.typeForm.dialogFormVisible = true;
+      this.typeForm.create = true;
+      console.log('cc');
+    },
+    filterInfo(data) {
+      let i = 1;
+      const filter = function (array) {
+        array.forEach(state => {
+          if (state.children) {
+            if (state.children.length === 0 || i === 2) {
+              delete state.children;
+            } else {
+              i += 1;
+              filter(state.children);
+            }
+          }
+        });
+        i = 1;
+      };
+      filter(data);
+    },
+    editType: function () {
+      console.log(this.typeForm.typeParams);
+      if (this.typeForm.typeParams.id) {
+        this.typeForm.dialogFormVisible = true;
+        this.typeForm.options = {
+          type: 'editType',
+          message: '编辑类别',
+          btn: '确认提交',
+          title: '编辑类别',
+        };
+        this.typeForm.create = false;
+        this.typeForm.form.name = this.typeForm.typeParams.name;
+        this.typeForm.form.id = this.typeForm.typeParams.id;
+        Material.detailType(this.typeForm.typeParams.id).then(res => {
+          this.typeForm.form = res.data;
+          delete this.typeForm.form.empId;
+          delete this.typeForm.form.createTime;
         }).catch(err => {
           console.log(err);
         });
       } else {
-        Employees.listOfStore(val).then(res => {
-          this.loading = false;
-          this.paginationData = res.data;
-          this.tbody = res.data.result;
-          this.conditions.pageSize = res.data.pageSize;
-          this.conditions.pageNo = res.data.page;
-        }).catch(err => {
-          console.log(err);
+        this.$message.error('请选中类别');
+      }
+      console.log('cc');
+    },
+    typeSave: function (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.request = true;
+          console.log(this.typeForm.form);
+          Material[this.typeForm.options.type].call(this, this.typeForm.form).then(res => {
+            if (res.status === 201) {
+              this.$message({
+                message: `${this.typeForm.options.message}成功`,
+                type: 'success',
+              });
+              this.typeForm.dialogFormVisible = false;
+              this.init();
+            }
+            return true;
+          }).catch(err => {
+            console.log(err);
+            this.$message({
+              message: err.msg,
+              type: 'error',
+            });
+          }).finally(() => {
+            this.request = false;
+          });
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+        return false;
+      });
+    },
+    deleteType: function () {
+      if (this.typeForm.typeParams.id) {
+        this.$confirm('此操作将删除该类别, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        }).then(() => {
+          Material.deleteType(this.typeForm.typeParams.id).then(res => {
+            if (res.status === 201) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+              });
+              this.init();
+            }
+          }).catch(err => {
+            this.handleError(err);
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除操作',
+          });
         });
+      } else {
+        this.$message.error('请选中类别');
       }
     },
     query: function (val) {
@@ -153,19 +374,17 @@ export default {
     handleCurrentChange: function (val) {
       this.paginationData.page = val;
     },
-    detail: function (val) {
-      this.$router.push(`/basic/employees/detail/${val}`);
-    },
-    edit: function () {
-      this.$router.push('/basic/employees/edit');
-    },
-    check: function () {
-      this.$router.push('/basic/department/list');
+    handleNodeClick(data) {
+      this.typeForm.typeParams = data;
+      this.getList({ typeId: data.id });
     },
   },
   computed: {
     conditionsWatch: function () {
       return this.paginationData.page;
+    },
+    dialogFormVisible: function () {
+      return this.typeForm.dialogFormVisible;
     },
   },
   components: {
@@ -178,10 +397,53 @@ export default {
         this.init(this.conditions);
       }
     },
+    dialogFormVisible: function (val) {
+      if (!val) {
+        this.typeForm.options.title = '';
+        this.typeForm.title = '';
+        for (const key in this.typeForm.form) {
+          if (Object.prototype.hasOwnProperty.call(this.typeForm.form, key)) {
+            this.typeForm.form[key] = '';
+          }
+        }
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.material-title{
+  font-size: 26px;
+}
+.tree-button{
+  padding: 15px;
+  border: 1px solid #d1dbe5;
+  border-bottom: none;
+}
+
+.tree-button button{
+  border: none;
+  font-size: 20px;
+  height: 20px;
+  padding-left: 6px;
+  padding-right: 6px;
+}
+
+.tree-button button.plus-btn{
+  color: #3599e6;
+}
+
+.tree-button button.edit-btn{
+  color: #f2c46c;
+}
+
+.tree-button button.delete-btn{
+  color: #f56767;
+}
+
+.page-oper .page-title{
+  font-size: 18px;
+}
 
 </style>
