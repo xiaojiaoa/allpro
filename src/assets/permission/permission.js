7addf14880b410fcc903ce702a2ca3f5dfe3182d
chen <983691551@qq.com>
2017-12-29 17:50:03 +0800

    【MOD】permission

--- src/assets/permission/permission.js
+++ src/assets/permission/permission.js
@@ -3,5 +3,7 @@ const permissionList = {
   900000: '/basic/cliques/add',
   900001: '/basic/cliques/edit',
   920002: '/basic/cliques/detail',
+  87101019: '/basic/cliques/add2',
+  871010: '/basic/cliques/add',
 };
 export default permissionList;
