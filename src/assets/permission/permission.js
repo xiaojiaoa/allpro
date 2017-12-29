bb012f351a691a12432b047266b1f53394f46fde
1651885856 <1651885856@qq.com>
2017-12-29 17:55:56 +0800

    【mod】permission.js

--- src/assets/permission/permission.js
+++ src/assets/permission/permission.js
@@ -3,5 +3,26 @@ const permissionList = {
   900000: '/basic/cliques/add',
   900001: '/basic/cliques/edit',
   920002: '/basic/cliques/detail',
+
+  98101029: '/basic/stores/addAll',
+  98101028: '/basic/stores/addSelf',
+  98101049: '/basic/stores/editAll',
+  98101048: '/basic/stores/editSelf',
+  98101039: '/basic/stores/forbid',
+
+  87101029: '/basic/role/addCliqueAll',
+  87101028: '/basic/role/addCliqueSelf',
+  87201029: '/basic/role/addStoreAll',
+  87201028: '/basic/role/addStoreSelf',
+
+  87101049: '/basic/role/editCliqueAll',
+  87101048: '/basic/role/editCliqueSelf',
+  87201049: '/basic/role/editStoreAll',
+  87201048: '/basic/role/editStoreSelf',
+
+  87101039: '/basic/role/forbidCliqueAll',
+  87101038: '/basic/role/forbidCliqueSelf',
+  87201039: '/basic/role/forbidStoreAll',
+  87201038: '/basic/role/forbidStoreSelf',
 };
 export default permissionList;
