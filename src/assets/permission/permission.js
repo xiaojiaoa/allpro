891adb27b380ea84f8cd5f1c19aa9d495c5176d7
1651885856 <1651885856@qq.com>
2018-01-03 09:27:46 +0800

    【mod】permission.js

--- src/assets/permission/permission.js
+++ src/assets/permission/permission.js
@@ -3,28 +3,59 @@ const permissionList = {
   900000: '/basic/cliques/add',
   900001: '/basic/cliques/edit',
   920002: '/basic/cliques/detail',
-
-  98101029: '/basic/stores/addAll',
-  98101028: '/basic/stores/addSelf',
-  98101049: '/basic/stores/editAll',
-  98101048: '/basic/stores/editSelf',
-  98101039: '/basic/stores/forbid',
-
-  87101029: '/basic/role/addCliqueAll',
-  87101028: '/basic/role/addCliqueSelf',
-  87201029: '/basic/role/addStoreAll',
-  87201028: '/basic/role/addStoreSelf',
-
-  87101049: '/basic/role/editCliqueAll',
-  87101048: '/basic/role/editCliqueSelf',
-  87201049: '/basic/role/editStoreAll',
-  87201048: '/basic/role/editStoreSelf',
-
-  87101039: '/basic/role/forbidCliqueAll',
-  87101038: '/basic/role/forbidCliqueSelf',
-  87201039: '/basic/role/forbidStoreAll',
-  87201038: '/basic/role/forbidStoreSelf',
-  87101019: '/basic/cliques/add2',
+  // 87101019: '/basic/cliques/add2',
   871010: '/basic/cliques/add',
+  // 门店
+  98101019: '/basic/stores/select19',
+  98101018: '/basic/stores/select18',
+  98101029: '/basic/stores/add29',
+  98101028: '/basic/stores/add28',
+  98101039: '/basic/stores/forbid39',
+  98101038: '/basic/stores/forbid38',
+  98101049: '/basic/stores/edit49',
+  98101048: '/basic/stores/edit48',
+  //  角色
+  87101019: '/basic/role/select19',
+  87101018: '/basic/role/select18',
+  87101029: '/basic/role/add29',
+  87101028: '/basic/role/add28',
+  87101039: '/basic/role/forbid39',
+  87101038: '/basic/role/forbid38',
+  87101049: '/basic/role/edit49',
+  87101048: '/basic/role/edit48',
+  87201019: '/basic/role/selectStore19',
+  87201018: '/basic/role/selectStore18',
+  87201029: '/basic/role/addStore29',
+  87201028: '/basic/role/addStore28',
+  87201039: '/basic/role/forbidStore39',
+  87201038: '/basic/role/forbidStore38',
+  87201049: '/basic/role/editStore49',
+  87201048: '/basic/role/editStore48',
+  //  订单受理-组合权限
+  93030201: '/order/orders/accepted01',
+
+  93020107: '/order/orders/select01',
+  93031101: '/order/orders/remark01',
+  93030801: '/order/orders/accept01',
+  93030901: '/order/orders/reback01',
+  93031201: '/order/orders/unlock01',
+  93031001: '/order/orders/reEdit01',
+  //  订单审核
+  93030401: '/order/orders/review01',
+  //  订单拆单
+  93030501: '/order/orders/apartOrder01',
+  //  订单拆审
+  93030601: '/order/orders/',
+  //  订单排料
+  93030701: '/order/orders/',
+  //  公告
+  88201000: '/basic/noticeInfo/add00',
+  88202000: '/basic/noticeInfo/del0',
+  88203000: '/basic/noticeInfo/edit00',
+  88104000: '/basic/noticeInfo/select00',
+
+  //  客户
+  95204001: '/basic/customers/area01',
+  95204000: '/basic/customers/select00',
 };
 export default permissionList;
