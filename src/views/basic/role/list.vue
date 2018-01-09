d6442fddf3fc3cbe3d200805df8a5902ee7d675c
1651885856 <1651885856@qq.com>
2018-01-09 13:42:43 +0800

    【mod】角色-查询-机构type

--- src/views/basic/role/list.vue
+++ src/views/basic/role/list.vue
@@ -141,7 +141,7 @@
             },
             {
               label: '机构',
-              type: 'select',
+              type: 'selectLinkage',
               field: 'bid',
               data: [],
               change: true,
