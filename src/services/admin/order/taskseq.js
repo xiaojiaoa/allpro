0b0eb237c8a67bec22d485f032fa9c8ec9971ae1
guyuqing <1260329072@qq.com>
2018-01-29 09:49:09 +0800

    【mod】流水类型管理

--- src/services/admin/order/taskseq.js
+++ src/services/admin/order/taskseq.js
@@ -7,7 +7,18 @@ const Taskseq = {
   list: (params) => Promise.resolve(adminServer.get('/api/taskflow/flow/page', { params: params })),
   // 根据门店号获取设计师
   designer: (bid) => Promise.resolve(adminServer.get(`/api/store/employee/designer/${bid}`)),
-
+  // 获取流水流程类型列表
+  typeList: (params) => Promise.resolve(adminServer.get('/api/taskflow/type/page', { params: params })),
+  // 获取流水功能模块列表
+  moduleList: (params) => Promise.resolve(adminServer.get('/api/taskflow/module/list', { params: params })),
+  // 获取可用的任务流水处理器
+  procList: (params) => Promise.resolve(adminServer.get('/api/taskflow/type/proc', { params: params })),
+  // 新增流水类型，包含流程
+  typeSave: (params) => Promise.resolve(adminServer.post('/api/taskflow/type', params, { headers: { 'Content-Type': 'application/json' } })),
+  // 新增流水类型，包含流程
+  typeDetail: (tskfTypeId) => Promise.resolve(adminServer.get(`/api/taskflow/type/detail?tskfTypeId=${tskfTypeId}`)),
+  // 禁用流水类型
+  typeDelete: (params) => Promise.resolve(adminServer.delete('/api/taskflow/type', { params: params })),
 };
 
 export default Taskseq;
